5a3c42e583c1c18225e01eda0fde23e17fcb531b
Alan-Balvin <tareauasbalvin@gmail.com>
2025-03-11 10:30:40 -0700

    scrollWrapper implementation

--- app/details.tsx
+++ app/details.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 
 export default function DetailScreen() {
@@ -13,7 +13,17 @@ export default function DetailScreen() {
       )}
       <Text style={styles.title}>{name}</Text>
       <Text style={styles.description}>{description}</Text>
-      <Text style={styles.longDescription}>{longDescription}</Text>
+
+      <View style={styles.scrollWrapper} >
+
+      <ScrollView >
+        <Text style={styles.longDescription} >
+
+        {longDescription}
+        </Text>
+        
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -24,5 +34,7 @@ const styles = StyleSheet.create({
   imagePlaceholder: { textAlign: 'center', color: '#888', marginBottom: 10 },
   title: { fontSize: 28, fontWeight: 'bold', marginBottom: 10 },
   description: { fontSize: 18, color: '#555' },
-  longDescription: { fontSize: 18, color: '#555'},
+  
+  scrollWrapper: { flex: 1, maxHeight: 300, marginTop: 10 }, 
+  longDescription: { fontSize: 16, color: '#555', textAlign: 'justify'},
 });
