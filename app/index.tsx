9c2c3837664276087c420378adf13cd25fc57610
Alan-Balvin <tareauasbalvin@gmail.com>
2025-03-20 18:28:27 -0700

    changes modules

--- app/index.tsx
+++ app/index.tsx
@@ -3,40 +3,11 @@ import { useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { attractions } from './data/attractions';
 import { styles } from './styles/styles';
+import { Navbar } from '@/components/Navbar';
 
 
 
 
-const menuOptions = [
-  { id: '1', title: 'Home', route: '/' },
-  { id: '2', title: 'Attractions', route: '/attractions' },
-  { id: '3', title: 'Restaurants', route: '/restaurants' },
-  { id: '4', title: 'Beaches', route: '/beaches' },
-  { id: '5', title: 'Events', route: '/events' },
-  { id: '6', title: 'Gallery', route: '/gallery' },
-  { id: '7', title: 'Map', route: '/map' },
-  { id: '8', title: 'Contact', route: '/contact' },
-];
-
-const Navbar = () => {
-  const router = useRouter();
-
-  return (
-    <View style={styles.navbar}>
-      <Text style={styles.navbarText}>Mazatlán Guide</Text>
-      <FlatList
-        horizontal
-        data={menuOptions}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => router.push(item.route)} style={styles.navItem}>
-            <Text style={styles.navText}>{item.title}</Text>
-          </TouchableOpacity>
-        )}
-      />
-    </View>
-  );
-};
 
 
 export default function HomeScreen() {
---
+++ components/Navbar.tsx
@@ -0,0 +1,35 @@
+import { View, Text, FlatList, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+import { styles } from '../app/styles/styles';
+
+
+const menuOptions = [
+  { id: '1', title: 'Home', route: '/' },
+  { id: '2', title: 'Attractions', route: '/attractions' },
+  { id: '3', title: 'Restaurants', route: '/restaurants' },
+  { id: '4', title: 'Beaches', route: '/beaches' },
+  { id: '5', title: 'Events', route: '/events' },
+  { id: '6', title: 'Gallery', route: '/gallery' },
+  { id: '7', title: 'Map', route: '/map' },
+  { id: '8', title: 'Contact', route: '/contact' },
+];
+
+export const Navbar = () => {
+  const router = useRouter();
+
+  return (
+    <View style={styles.navbar}>
+      <Text style={styles.navbarText}>Mazatlán Guide</Text>
+      <FlatList
+        horizontal
+        data={menuOptions}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => router.push(item.route)} style={styles.navItem}>
+            <Text style={styles.navText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
